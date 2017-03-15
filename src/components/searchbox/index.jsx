38399de4bef44eef46265b5fc27357e0e1a74644
Alexander Quirk <alexanderquirk@alex-quirk-macbook.local>
2017-03-15 09:55:04 -0400

    Style searchbox

--- src/components/searchbox/index.jsx
+++ src/components/searchbox/index.jsx
@@ -2,10 +2,12 @@ import React, {PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
+import {noop} from '../../utils/func_utils';
+
 import {update_query_search_string} from '../../actions/query_actions';
 import {fetch_forecast_for_city} from '../../actions/weather_data_actions';
 
-import {noop} from '../../utils/func_utils';
+import './searchbox.scss';
 
 export const unconnected_searchbox_component = Object.assign(
   ({
@@ -14,18 +16,25 @@ export const unconnected_searchbox_component = Object.assign(
     search_for_city_on_click = () => noop,
     search_for_city_on_press_enter = () => noop
   }) =>
-    <div className='searchbox-component'>
-      <input
-        type = 'text'
-        value = {query_search_string}
-        onChange = {handle_searchbox_change}
-        onKeyUp = {search_for_city_on_press_enter.apply ? search_for_city_on_press_enter(query_search_string) : noop}
-        className = 'searchbox'
-      />
-      <button
-        className="submit"
+    <div className='searchbox-component row'>
+      <div className='col-lg-10 col-md-12'>
+        <div className="searchbox input-group">
+          <input
+            placeholder='City Name Query'
+            type = 'text'
+            value = {query_search_string}
+            onChange = {handle_searchbox_change}
+            onKeyUp = {search_for_city_on_press_enter.apply ? search_for_city_on_press_enter(query_search_string) : noop}
+          />
+          <div className='underline-container'/>
+        </div>
+      </div>
+      <div className='col-lg-2 col-md-12'>
+        <button
+        className="btn btn-primary submit"
         onClick={search_for_city_on_click.apply ? search_for_city_on_click(query_search_string) : noop}
-      >Search</button>
+        >Search</button>
+      </div>
     </div>,
   {
     propTypes: {
