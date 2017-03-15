284b98cd34f6ab3b8287edf8beea535c724eee50
quirkles <al.quirk@gmail.com>
2017-03-15 18:51:42 -0400

    Flesh out panel tests

--- src/components/weekly_summary/index.jsx
+++ src/components/weekly_summary/index.jsx
@@ -1,18 +1,40 @@
 import React, {PropTypes} from 'react';
+import classnames from 'classnames';
+
+import noop from '../../utils/func_utils';
 
 import './weekly_summary.scss';
 
 export default Object.assign(
   ({
-    weekly_data = [1,3,3]
+    weekly_data = null,
+    is_open = false,
+    toggle_weekly_summary = noop
   }) =>
+  <div className='weekly-summary-component'>
     <div
-      className={`weekly-summary-component ${weekly_data && weekly_data.length && weekly_data.map ? '' : 'no-data'}`}
+      className={classnames(
+        'weekly-summary-pane', {
+           'open': is_open === true,
+           'no-data': !(weekly_data && weekly_data.length && weekly_data.map)
+         })}
     >
-      <div className='weekly-summary-toggler'>UP</div>
-    </div>,
+      <div
+        className='weekly-summary-toggler serif'
+        onClick={toggle_weekly_summary}
+      >
+        {is_open === true ? 'X' : '^'}
+      </div>
+    </div>
+    <div
+      className='weekly-summary-overlay'
+      onClick={toggle_weekly_summary}
+    ></div>
+  </div>,
   {
     propTypes: {
-      weekly_data: PropTypes.array
+      weekly_data: PropTypes.array,
+      is_open: PropTypes.bool,
+      toggle_weekly_summary: PropTypes.func
     }
   });
--- test/components/weekly_summary_component_spec.jsx
+++ test/components/weekly_summary_component_spec.jsx
@@ -10,14 +10,15 @@ import WeeklySummary from '../../src/components/weekly_summary';
 describe('Weekly summary component', () => {
   it('Renders with no props, had the no-data class when given no data', () => {
     const wrapper = shallow(<WeeklySummary/>);
-    expect(wrapper.find('.weekly-summary-component.no-data')).to.have.length(1);
+    expect(wrapper.find('.weekly-summary-pane.no-data')).to.have.length(1);
+    expect(wrapper.find('.weekly-summary-pane.open')).to.have.length(0);
   });
 
   it('Accepts a weekly_data prop, does not have no-data class when given data', () => {
     const error_spy = spy();
     stub(console, 'error', error_spy);
     const wrapper = shallow(<WeeklySummary weekly_data={[{}, {}]}/>);
-    expect(wrapper.find('.weekly-summary-component.no-data')).to.have.length(0);
+    expect(wrapper.find('.weekly-summary-pane.no-data')).to.have.length(0);
     console.error.restore();
     expect(error_spy.callCount).to.equal(0);
   });
@@ -26,7 +27,45 @@ describe('Weekly summary component', () => {
     const error_spy = spy();
     stub(console, 'error', error_spy);
     const wrapper = shallow(<WeeklySummary weekly_data='bad'/>);
-    expect(wrapper.find('.weekly-summary-component.no-data')).to.have.length(1);
+    expect(wrapper.find('.weekly-summary-pane.no-data')).to.have.length(1);
+    console.error.restore();
+    expect(error_spy.callCount).to.equal(1);
+  });
+
+  it('Accepts an is_open, rendered with open class if set to true', () => {
+    const error_spy = spy();
+    stub(console, 'error', error_spy);
+    const wrapper = shallow(<WeeklySummary is_open={true}/>);
+    expect(wrapper.find('.weekly-summary-pane.open')).to.have.length(1);
+    console.error.restore();
+    expect(error_spy.callCount).to.equal(0);
+  });
+
+  it('Accepts an is_open prop, warns of invalid proptype if is open is not a bool', () => {
+    const error_spy = spy();
+    stub(console, 'error', error_spy);
+    const wrapper = shallow(<WeeklySummary is_open='bad'/>);
+    expect(wrapper.find('.weekly-summary-pane.open')).to.have.length(0);
+    console.error.restore();
+    expect(error_spy.callCount).to.equal(1);
+  });
+
+  it('Accepts a toggle_weekly_summary callback as a prop, no warning if function, called if toggler or overlay clicked', () => {
+    const error_spy = spy();
+    const toggle_spy = spy();
+    stub(console, 'error', error_spy);
+    const wrapper = shallow(<WeeklySummary toggle_weekly_summary={toggle_spy}/>);
+    wrapper.find('.weekly-summary-component .weekly-summary-toggler').simulate('click');
+    wrapper.find('.weekly-summary-component .weekly-summary-overlay').simulate('click');
+    console.error.restore();
+    expect(error_spy.callCount).to.equal(0);
+    expect(toggle_spy.callCount).to.equal(2);
+  });
+
+  it('Accepts a toggle_weekly_summary callback as a prop, warning if not function', () => {
+    const error_spy = spy();
+    stub(console, 'error', error_spy);
+    shallow(<WeeklySummary toggle_weekly_summary='bad'/>);
     console.error.restore();
     expect(error_spy.callCount).to.equal(1);
   });
